64aefbacc50a47de520e11f018de4cb134bc64a9
Alexandre Bléron <alex.bleron@gmail.com>
2025-02-13 23:28:15 +0100

    Implement std::error::Error for Error

--- src/lib.rs
+++ src/lib.rs
@@ -48,6 +48,14 @@ impl std::fmt::Debug for Error {
 	}
 }
 
+impl std::fmt::Display for Error {
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		std::fmt::Debug::fmt(self, f)
+	}
+}
+
+impl std::error::Error for Error {}
+
 pub type Result<T> = std::result::Result<T, Error>;
 
 pub(crate) fn succeeded(result: sys::SlangResult) -> bool {
