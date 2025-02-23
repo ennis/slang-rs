42329b44ebc1a1e0f2e818e9f187a70aacfc3759
Alexandre Bléron <alex.bleron@gmail.com>
2025-02-23 20:27:44 +0100

    impl Error for Error (lost in merge)

--- src/lib.rs
+++ src/lib.rs
@@ -48,6 +48,13 @@ impl std::fmt::Debug for Error {
 		}
 	}
 }
+impl std::fmt::Display for Error {
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		std::fmt::Debug::fmt(self, f)
+	}
+}
+
+impl std::error::Error for Error {}
 
 pub type Result<T> = std::result::Result<T, Error>;
 
